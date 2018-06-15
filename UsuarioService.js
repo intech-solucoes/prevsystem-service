9843ab4ea30fa467ea5c5543bb34604fbc021017
Luis Bruno <l_brunofidelis@yahoo.com.br>
2018-06-15 18:02:37 -0300

    Método de primeiro acesso
    Método de primeiro acesso para rota '/usuario/criarAcesso'.

--- UsuarioService.js
+++ UsuarioService.js
@@ -8,6 +8,11 @@ class UsuarioService extends BaseService {
     Login(cpf, senha) {
         return this.CriarRequisicao("POST", "/usuario/login", { Cpf: cpf, Senha: senha });
     }
+
+    PrimeiroAcesso(cpf, dataNascimento) {
+        this.CriarRequisicao("POST", "/usuario/criarAcesso", { Cpf: cpf, DataNascimento: dataNascimento });
+    }
+
 }
 
 export default new UsuarioService();
