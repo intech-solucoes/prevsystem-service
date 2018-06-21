0297bb7f674c1be2ed565865528272dae319a245
Luis Bruno <l_brunofidelis@yahoo.com.br>
2018-06-21 10:23:34 -0300

    Busca em PlanoService
    Busca por fundação, empresa, plano e referência.

--- PlanoService.js
+++ PlanoService.js
@@ -13,6 +13,13 @@ class PlanoService extends BaseService {
         return this.CriarRequisicao("GET", `/plano/porFundacaoEmpresaPlano/${fundacao}/${empresa}/${plano}`, null)
     }
 
+    BuscarPorFundacaoEmpresaPlanoReferencia(dados) {
+        var fundacao = localStorage.getItem("fundacao");
+        var empresa = localStorage.getItem("empresa");
+
+        return this.CriarRequisicao("GET", `/plano/relatorioExtratoPorFundacaoEmpresaPlanoReferencia/${fundacao}/${empresa}/${dados.plano}/${dados.dataInicio}/${dados.dataFim}`, null);
+    }
+
 }
 
 export default new PlanoService();
