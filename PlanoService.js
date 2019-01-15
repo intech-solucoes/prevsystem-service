8746ddfe555b764fddedd4a2c46c2c8c2e080288
Rony Moura <ronymmoura@gmail.com>
2019-01-15 19:37:03 -0200

    [PREVES] Ajustes

--- PlanoService.js
+++ PlanoService.js
@@ -13,16 +13,33 @@ class PlanoService extends BaseService {
         return this.CriarRequisicao("GET", `/plano/porCodigo/${plano}`)
     }
 
-    RelatorioExtratoPorPlanoReferencia(plano, dataInicio, dataFim) {
-        return this.CriarRequisicaoBlob("GET", `/plano/relatorioExtratoPorPlanoReferencia/${plano}/${dataInicio}/${dataFim}`);
+    RelatorioExtratoPorPlanoReferencia(plano, dataInicio, dataFim, enviarPorEmail = false) {
+        if(enviarPorEmail)
+            return this.CriarRequisicao("GET", `/plano/relatorioExtratoPorPlanoReferencia/${plano}/${this.FormatarData(dataInicio)}/${this.FormatarData(dataFim)}/${enviarPorEmail}`);
+        else
+            return this.CriarRequisicaoBlob("GET", `/plano/relatorioExtratoPorPlanoReferencia/${plano}/${this.FormatarData(dataInicio)}/${this.FormatarData(dataFim)}/${enviarPorEmail}`);
     }
 
-    RelatorioCertificado(plano) {
-        return this.CriarRequisicaoBlob("GET", `/plano/certificado/${plano}`);
+    RelatorioCertificado(plano, enviarPorEmail = false) {
+        if(enviarPorEmail)
+            return this.CriarRequisicao("GET", `/plano/certificado/${plano}/${enviarPorEmail}`);
+        else
+            return this.CriarRequisicaoBlob("GET", `/plano/certificado/${plano}/${enviarPorEmail}`);
     }
 
     BuscarPorEmpresa(empresa) {
         return this.CriarRequisicao("GET", `/plano/porEmpresa/${empresa}`);
+    }
+
+    RelatorioCertificadoSeguro(enviarPorEmail = false) {
+        if(enviarPorEmail)
+            return this.CriarRequisicao("GET", `/plano/certificadoSeguro/${enviarPorEmail}`);
+        else
+            return this.CriarRequisicaoZip("GET", `/plano/certificadoSeguro/${enviarPorEmail}`);
+    }
+
+    PossuiCertificadoSeguro() {
+        return this.CriarRequisicao("GET", `/plano/possuiCertificadoSeguro`);
     }
 
 }
