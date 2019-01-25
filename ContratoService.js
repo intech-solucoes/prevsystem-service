b7acf7f3a3b3e7be2bec4f0fc4e683745783ecfb
Israel Correia de Castro <israel.correia@gmail.com>
2019-01-25 11:42:23 -0200

    Ajustes no ContratoService

--- ContratoService.js
+++ ContratoService.js
@@ -1,6 +1,7 @@
 import BaseService from "@intechprev/service";
 
 class ContratoService extends BaseService {
+    
     SabesprevBuscarAtivosPorPlano(cdPlano) {
         return this.CriarRequisicao("GET", `/contrato/sabesprevAtivosPorPlano/${cdPlano}`);
     }
@@ -12,7 +13,7 @@ class ContratoService extends BaseService {
     ParametrosPorPlano(cdPlano) {
         return this.CriarRequisicao("GET", `/contrato/parametrosPorPlano/${cdPlano}`);
     }
-
+    
     BuscarConcessao(cdPlano, cdModal, cdNatur, dataCredito) {
         return this.CriarRequisicao("GET", `/contrato/buscarConcessao/${cdPlano}/${cdModal}/${cdNatur}/${this.FormatarData(dataCredito)}`);
     }
@@ -20,6 +21,19 @@ class ContratoService extends BaseService {
     BuscarParametrosParcelas(CD_PLANO, CD_MODAL, CD_NATUR, valorSolicitado, concessao) {
         return this.CriarRequisicao("POST", `/contrato/parametrosParcelas`, { CD_PLANO, CD_MODAL, CD_NATUR, valorSolicitaco, concessao });
     }
+
+    BuscarParcelas(params){
+        return this.CriarRequisicao("POST", `/contrato/parametrosParcelas`, params);
+    }
+
+    SendToken(enviarEmail, enviarSMS){
+        return this.CriarRequisicao("POST", `/contrato/gerarToken/${enviarEmail}/${enviarSMS}`,{});
+    }
+
+    Contratar(params){
+        return this.CriarRequisicao("POST", `/contrato/contratar`,params);
+    }
+    
 }
 
 export default new ContratoService();
