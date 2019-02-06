114daf99cd75322c0a1f16e7d7c0e93d735bce5b
Rony <ronymmoura@gmail.com>
2019-02-05 22:09:15 -0200

    Criando pesquisa de funcionário

--- FuncionarioService.js
+++ FuncionarioService.js
@@ -4,6 +4,10 @@ class FuncionarioService extends BaseService {
     Buscar() {
         return this.CriarRequisicao("GET", "/funcionario");
     }
+
+    Pesquisar(CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA) {
+        return this.CriarRequisicao("POST", "/funcionario/pesquisar", { CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA });
+    }
 }
 
 export default new FuncionarioService();
