abd0cbf559616eb2c90050b3e4eeb547822e136f
Rony <ronymmoura@gmail.com>
2019-02-06 10:51:45 -0200

    Incluindo nome na pesquisa de funcionario

--- FuncionarioService.js
+++ FuncionarioService.js
@@ -5,8 +5,8 @@ class FuncionarioService extends BaseService {
         return this.CriarRequisicao("GET", "/funcionario");
     }
 
-    Pesquisar(CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA) {
-        return this.CriarRequisicao("POST", "/funcionario/pesquisar", { CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA });
+    Pesquisar(CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA, NOME) {
+        return this.CriarRequisicao("POST", "/funcionario/pesquisar", { CD_FUNDACAO, CD_EMPRESA, CD_PLANO, CD_SIT_PLANO, NUM_MATRICULA, NOME });
     }
 }
 
